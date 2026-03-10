c185d28b90fce465322f6bd0e5b4d59b7c0ee89b
Nola Ristiyanti <nolaristiyanti.com>
2026-03-10 22:48:52 +0700

    update code

--- index.php
+++ index.php
@@ -6,8 +6,12 @@
     function tampilkanProfil($nama, $usia, $statusAktif, $hobi): void {
         echo "Nama : " . $nama . PHP_EOL;
         echo "Usia : " . $usia . PHP_EOL;
-        echo "Saldo : " . $saldo . PHP_EOL;
+        $status = $statusAktif ? "Aktif" : "Tidak aktif";
+        echo "Status : " . $status . PHP_EOL;
         echo "Hobi : ";
+        foreach($hobi as $h) {
+            echo $h . " ";
+        }
     }
     $nama = "Nola";
     $usia = 26;
@@ -16,13 +20,12 @@
     $hobi = ["Memasak", "Karaoke"];
 
     echo "Soal 1 - Profil User Sederhana (Introduction, Syntax, Variable & Data Type)" . PHP_EOL;
-    echo "Nama : " . $nama . PHP_EOL;
-    echo "Usia : " . $usia . PHP_EOL;
-    echo "Saldo : " . $saldo . PHP_EOL;
-    echo "Hobi : ";
-    foreach($hobi as $h) {
-        echo $h . " ";
-    }
-    echo PHP_EOL;
+    echo $nama . PHP_EOL;
+    echo $usia . PHP_EOL;
+    echo $saldo . PHP_EOL;
+    var_dump($hobi);
     var_dump($statusAktif);
+    echo PHP_EOL;
+
+    tampilkanProfil($nama, $usia, $statusAktif, $hobi);
 ?>
